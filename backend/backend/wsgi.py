9931a5ea58e8d74b1c9168d9c50b10a3c28fb067
Ashbin Rajbomsi <rbashbin1@gmail.com>
2026-06-20 23:49:51 +0545

    Fix sys.path level - was off by one

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 
 # Add the project root (the folder containing manage.py) to sys.path
-sys.path.insert(0, str(Path(__file__).resolve().parent.parent.parent))
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
